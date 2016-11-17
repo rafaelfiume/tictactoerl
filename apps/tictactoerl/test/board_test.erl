9d853bde1883086877b046ed8f81b85b254748d2
Rafael Fiume <rfiume@equalexperts.com>
2016-11-17 18:17:21 +0000

    Clearer test names
    @rafaelfiume

--- apps/tictactoerl/test/board_test.erl
+++ apps/tictactoerl/test/board_test.erl
@@ -86,47 +86,47 @@ wins_with_a_ascendent_diagonal_row_test() ->
 %%%       Board Position      %%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-maps_user_input_1_to_correct_board_position_test() ->
+mark_position_when__topleft__is_available_test() ->
     PreviousBoad = #board_table{},
     CurrentBoard = board:mark_position_if_available(PreviousBoad, 1, "X"),
     CurrentBoard = #board_table{top_left = "X"}.
 
-maps_user_input_2_to_correct_board_position_test() ->
+mark_position_when__topcenter__is_available_test() ->
     PreviousBoad = #board_table{},
     CurrentBoard = board:mark_position_if_available(PreviousBoad, 2, "X"),
     CurrentBoard = #board_table{top_center = "X"}.
 
-maps_user_input_3_to_correct_board_position_test() ->
+mark_position_when__topright__is_available_test() ->
     PreviousBoad = #board_table{},
     CurrentBoard = board:mark_position_if_available(PreviousBoad, 3, "X"),
     CurrentBoard = #board_table{top_right = "X"}.
 
-maps_user_input_4_to_correct_board_position_test() ->
+mark_position_when__midleft__is_available_test() ->
     PreviousBoad = #board_table{},
     CurrentBoard = board:mark_position_if_available(PreviousBoad, 4, "X"),
     CurrentBoard = #board_table{mid_left = "X"}.
 
-maps_user_input_5_to_correct_board_position_test() ->
+mark_position_when__center__is_available_test() ->
     PreviousBoad = #board_table{},
     CurrentBoard = board:mark_position_if_available(PreviousBoad, 5, "X"),
     CurrentBoard = #board_table{center = "X"}.
 
-maps_user_input_6_to_correct_board_position_test() ->
+mark_position_when__midright__is_available_test() ->
     PreviousBoad = #board_table{},
     CurrentBoard = board:mark_position_if_available(PreviousBoad, 6, "X"),
     CurrentBoard = #board_table{mid_right = "X"}.
 
-maps_user_input_7_to_correct_board_position_test() ->
+mark_position_when__bottomleft__is_available_test() ->
     PreviousBoad = #board_table{},
     CurrentBoard = board:mark_position_if_available(PreviousBoad, 7, "X"),
     CurrentBoard = #board_table{bottom_left = "X"}.
 
-maps_user_input_8_to_correct_board_position_test() ->
+mark_position_when__bottomcenter__is_available_test() ->
     PreviousBoad = #board_table{},
     CurrentBoard = board:mark_position_if_available(PreviousBoad, 8, "X"),
     CurrentBoard = #board_table{bottom_center = "X"}.
 
-maps_user_input_9_to_correct_board_position_test() ->
+mark_position_when__bottomright__is_available_test() ->
     PreviousBoad = #board_table{top_left = "X", center = "O", top_right = "X"},
     CurrentBoard = board:mark_position_if_available(PreviousBoad, 9, "O"),
     CurrentBoard = #board_table{top_left = "X", center = "O", top_right = "X", bottom_right = "O"}.
