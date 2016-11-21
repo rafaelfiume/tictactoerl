b158507fd5dd23ba707f46f24bd9996714d50abd
Rafael Fiume <rfiume@equalexperts.com>
2016-11-21 17:55:32 +0000

    Improves tests using a Test Generator Function
        - It also slightly changes the regex used to validate user inputs
    
    @rafaelfiume

--- apps/tictactoerl/src/board_position.erl
+++ apps/tictactoerl/src/board_position.erl
@@ -4,7 +4,7 @@
 
 user_input(Input) when is_list(Input) -> user_input(iolist_to_binary(Input));
 user_input(Input) when is_binary(Input) ->
-    case re:run(Input, "^([1-9])(\\n)?$", [{capture, [1], list}]) of
+    case re:run(Input, "^([1-9])$", [{capture, [1], list}]) of
         {match, Captured} ->
             [H|_] = Captured, 
             list_to_integer(H);
--- apps/tictactoerl/test/board_position_test.erl
+++ apps/tictactoerl/test/board_position_test.erl
@@ -1,19 +1,23 @@
 -module(board_position_test).
 -include_lib("eunit/include/eunit.hrl").
 
-maps_user_input_to_correct_board_position_test() ->
-    1 = board_position:user_input("1"),
-    9 = board_position:user_input("9"),
-    1 = board_position:user_input("1\n"),
-    2 = board_position:user_input("2\n"),
-    3 = board_position:user_input("3\n"),
-    4 = board_position:user_input("4\n"),
-    5 = board_position:user_input("5\n"),
-    6 = board_position:user_input("6\n"),
-    7 = board_position:user_input("7\n"),
-    8 = board_position:user_input("8\n"),
-    9 = board_position:user_input("9\n"),
-    unknown = board_position:user_input("k\n"),
-    unknown = board_position:user_input("10"),
-    unknown = board_position:user_input("10\n").
-
+% test generator function
+maps_user_input_to_correct_board_position_test_() ->
+    [ % test generators
+     ?_assertEqual(1, board_position:user_input("1")),
+     ?_assertEqual(9, board_position:user_input("9")),
+     ?_assertEqual(1, board_position:user_input("1\n")),
+     ?_assertEqual(2, board_position:user_input("2\n")),
+     ?_assertEqual(3, board_position:user_input("3\n")),
+     ?_assertEqual(4, board_position:user_input("4\n")),
+     ?_assertEqual(5, board_position:user_input("5\n")),
+     ?_assertEqual(6, board_position:user_input("6\n")),
+     ?_assertEqual(7, board_position:user_input("7\n")),
+     ?_assertEqual(8, board_position:user_input("8\n")),
+     ?_assertEqual(9, board_position:user_input("9\n")),
+     ?_assertEqual(unknown, board_position:user_input("k\n")),
+     ?_assertEqual(unknown, board_position:user_input("10")),
+     ?_assertEqual(unknown, board_position:user_input("10\n")),
+     ?_assertEqual(unknown, board_position:user_input("11"))
+    ].
+    
