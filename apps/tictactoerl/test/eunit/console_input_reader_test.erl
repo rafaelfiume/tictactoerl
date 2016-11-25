80a0ed95ab70299fa5a792158f24830e7ec561d6
Rafael Fiume <rfiume@equalexperts.com>
2016-11-25 14:27:41 +0000

    Moves eunit tests and ct tests to their correspondent folders
    @rafaelfiume

--- apps/tictactoerl/test/eunit/console_input_reader_test.erl
+++ apps/tictactoerl/test/eunit/console_input_reader_test.erl
@@ -10,8 +10,12 @@ sends_an_event_to_fsm_when_receiving_input_from_user_test() ->
     
     whenn(fun() -> console_input_reader:read_user_input(Parent) end).
 
+%%%%%%%%%%%%%%%
+%%% HELPERS %%%
+%%%%%%%%%%%%%%%
+
 given_that_user_types(UserInput) ->
-    code:unstick_dir(filename:dirname(code:where_is_file("io.beam"))), %% TODO 24/Is it possible to replace code:unstick by a similar meck functionality?
+    code:unstick_dir(filename:dirname(code:where_is_file("io.beam"))), 
     meck:new(io, [passthrough, no_link]),
     meck:expect(io, get_line, fun(_Prompt) -> UserInput end).
 
