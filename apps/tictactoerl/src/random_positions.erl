9a4636f5bc2779079bf6034768360a5ebca74efd
Rafael Fiume <rafael.fiume@yahoo.it>
2016-11-26 12:22:28 +0000

    Plays the game in BOT mode (wip)
        - Second version of component to generate random positions
    
    @rafaelfiume

--- apps/tictactoerl/src/random_positions.erl
+++ apps/tictactoerl/src/random_positions.erl
@@ -1,10 +1,27 @@
 -module(random_positions).
 
--export([next/1]).
+-export([new/0, next/1]).
 
-next(ListOfNumbers) ->
+new() ->
+    spawn_link(fun() -> loop([]) end).
+
+next(Pid) when is_pid(Pid) ->
+    Pid ! {next_number, self()},
+    receive V -> V end;
+
+% This will loop forever if ListOfNumbers has 9 numbers or more.
+% Trusting the client this is not going to happen
+next(ListOfNumbers) when is_list(ListOfNumbers) ->
     Number = rand:uniform(9),
     case Number =/= 0 andalso not lists:member(Number, ListOfNumbers) of
-        true -> [Number|ListOfNumbers];
+        true -> Number;
         false -> next(ListOfNumbers)
+    end.
+
+loop(ListOfNumbers) ->
+    receive
+        {next_number, From} ->
+            RandomNumber = next(ListOfNumbers),
+            From ! RandomNumber,
+            loop([RandomNumber|ListOfNumbers])
     end.
--- apps/tictactoerl/test/eunit/random_positions_test.erl
+++ apps/tictactoerl/test/eunit/random_positions_test.erl
@@ -1,12 +1,23 @@
 -module(random_positions_test).
 -include_lib("eunit/include/eunit.hrl").
 
+receives_a_random_number_from_one_to_nine_test() ->
+    Pid = random_positions:new(),
+
+    FirstResult = random_positions:next(Pid),
+    SecondResult = random_positions:next(Pid),
+
+    ?assert(1 =< FirstResult),  ?assert(FirstResult =< 9),
+    ?assert(1 =< SecondResult), ?assert(SecondResult =< 9),
+    ?assert(FirstResult =/= SecondResult).
+
+% TODO : 26/11/2016 : It fills we're testing the internals here...
 genetates_9_different_numbers_randomly_one_time_each_from_1_to_9_test() ->
     ListOfNumbers = [2, 3, 5, 9, 6, 1, 8], % missing 4 and 7
 
-    FinalListOfNumbers = random_positions:next(
-        random_positions:next(ListOfNumbers)
-    ),
+    ARandomNumber = random_positions:next(ListOfNumbers),
+    AnotherRandomNumber = random_positions:next([ARandomNumber|ListOfNumbers]),
 
-    ?assertEqual(9, length(FinalListOfNumbers)),
-    ?assertEqual([1,2,3,4,5,6,7,8,9], lists:sort(FinalListOfNumbers)).
+    MissingNumbers = [ARandomNumber, AnotherRandomNumber],
+    ?assertEqual(2, length(MissingNumbers)),
+    ?assertEqual([4, 7], lists:sort(MissingNumbers)).
